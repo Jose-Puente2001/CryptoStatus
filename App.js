9919cc35a6bdd30703789b1e09c6d0a3d438501c
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2021-09-23 10:48:23 -0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, StyleSheet, FlatList, Image } from 'react-native';
+import CoinItem from "./components/CoinItem";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () =>{
+
+const [coins, setCoins] = useState([])
+
+
+const Getdata = async () =>{
+
+
+   const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+   const json = await res.json();
+   setCoins(json)
+
+}
+
+ useEffect(()=>{
+ 
+     Getdata()
+},[])
+
+	return(
+		
+		<View style={styles.container}>
+     	<FlatList 
+          data={coins}
+          renderItem={({ item }) =>  {
+          	return <CoinItem coin={item} />
+          	}}
+          	
+     	/>
+     </View>
+  
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+ container: {
+ 	backgroundColor: "#141414",
+ 	alignItems: "center",
   },
-});
+
+})
+
+
+
+export default App;
---
+++ components/CoinItem.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { Text, View, StyleSheet, FlatList, Image } from 'react-native';
+
+
+const CoinItem = ({ coin })=>{
+
+  return(
+  	<View style={styles.container}>
+      <View style={styles.containerNames}>
+                     <Image  source={{uri: coin.image}} style={{height: 30, width: 30}}/>
+                     <Text style={styles.text}>{coin.name}</Text>
+                  </View>
+                  <Text style={styles.textprice}>${coin.current_price}</Text>
+                  </View>
+
+
+  )
+}
+
+
+const styles = StyleSheet.create({
+ container: {
+ 	backgroundColor: "#121212",
+ 	paddingTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  containerNames: {
+    flexDirection: "row",
+  },
+
+  text: {
+     color: "#fff",
+  },
+
+  textprice: {
+
+  	fontBold: "bold",
+  	color: "#fff",
+    marginLeft: 40,
+
+  },
+
+})
+
+export default CoinItem
